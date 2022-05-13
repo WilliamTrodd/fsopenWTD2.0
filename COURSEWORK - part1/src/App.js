fb5f47dcb8439f8634e09b6ef6d83d22b7d87fd4
WilliamTrodd <william.trodd@gmail.com>
2022-05-13 12:03:52 +0300

    part1c complete
    Coursework code updated to reflect state, buttons, event handlers and refactoring

--- COURSEWORK - part1/src/App.js
+++ COURSEWORK - part1/src/App.js
@@ -1,17 +1,41 @@
-const Hello = (props) => {
+import { useState } from 'react'
+
+const Hello = ({name,age}) => {
+  const bornYear = () => new Date().getFullYear() - age
+
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
+      <p>Hello {name}, you are {age} years old</p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   )
 }
 
-const App = () => {
-  const name = "Will"
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({onClick, text}) => (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+
+
+const App = (props) => {
+  const [ counter, setCounter ] = useState(0)
   const age = 10
+  const incOne = () => setCounter(counter+1)
+  const setToZero = () => setCounter(0)
+  const decOne = ()  => setCounter(counter-1)
+
+  const name = "Will"
   return (
     <div>
       <h1>Greetings</h1>
+
+      <Display counter={counter} />
+      <Button onClick={incOne} text='plus' />
+      <Button onClick={decOne} text='minus' />
+      <Button onClick={setToZero} text='zero' />
       <Hello name={name} age={age +16}/>
       <Hello name="Tester" age={age}/>
     </div>
--- part1/courseinfo/courseinfo/src/App.js
+++ part1/courseinfo/courseinfo/src/App.js
@@ -9,9 +9,9 @@ const Header = (props) => {
 const Content = (props) => {
   return(
     <>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
     </>
   )
 }
@@ -24,27 +24,42 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
+  let sum = 0
+  props.parts.forEach(part => {
+    sum += part.exercises
+  });
+
   return(
     <>
-      <p>Number of exercises {props.e1+props.e2+props.e3}</p>
+      <p>Number of exercises {sum}</p>
     </>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name:'Half Stack application development',
+    parts: [
+      {
+      name: 'Fundamentals of React',
+      exercises: 10
+      },
+      {
+      name:'Using props to pass data',
+      exercises: 7
+      },
+      {
+      name: 'State of a component',
+      exercises: 14
+      }
+    ]
+  }
 
   return(
     <div>
-      <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-      <Total e1={exercises1} e2={exercises2} e3={exercises3} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 
