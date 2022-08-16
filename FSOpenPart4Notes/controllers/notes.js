518e903595d5435721721e0e41f62fe2060129e6
WilliamTrodd <william.trodd@gmail.com>
2022-08-16 17:06:11 +0200

    Updated CW4 to FSOpenPart4Notes

---
+++ FSOpenPart4Notes/controllers/notes.js
@@ -0,0 +1,52 @@
+const notesRouter = require('express').Router()
+const Note = require('../models/note')
+
+notesRouter.get('/', async (request, response) => {
+  const notes = await Note.find({})
+  response.json(notes)
+})
+
+notesRouter.get('/:id', async (request, response) => {
+  const note = await Note.findById(request.params.id)
+  note ? response.json(note) : response.status(404).end()
+})
+
+notesRouter.post('/', async (request, response) => {
+  const body = request.body
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
+
+  const savedNote = await note.save()
+  response.status(201).json(savedNote)
+
+})
+
+notesRouter.delete('/:id', async (request, response) => {
+
+  await Note.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+
+})
+
+notesRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+
+  Note.findByIdAndUpdate(request.params.id, note, { new:true })
+    .then(updatedNote => {
+      console.log(updatedNote)
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = notesRouter
