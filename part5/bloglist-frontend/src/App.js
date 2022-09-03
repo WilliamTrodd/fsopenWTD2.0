a7859c825fdc57f687d07e4b60c5e993e27cb865
WilliamTrodd <william.trodd@gmail.com>
2022-09-03 16:45:38 +0300

    Blogs sorted by desc. likes

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -125,7 +125,7 @@ const App = () => {
           <Togglable buttonLabel = 'create new blog' ref={blogFormRef}>
             <br/><BlogForm createBlog={createBlog}/><br/>
           </Togglable>
-        {blogs.map(blog =>
+        {blogs.sort((a,b) => b.likes-a.likes).map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
         </div>
