eae04f7885537b4381338899107faf38072f093c
WilliamTrodd <william.trodd@gmail.com>
2022-10-17 07:13:22 +0300

    7.7 complete

--- part7/countries/src/App.js
+++ part7/countries/src/App.js
@@ -18,7 +18,16 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+    axios
+      .get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+      .then(response => {
+        setCountry(response.data[0])
+      })
+      .catch(error => {
+        setCountry(null)
+      })
+  },[name])
 
   return country
 }
